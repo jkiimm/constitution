62c3dd7e2b5832a394033b5bf5b2885c78c0fe5c
Jinyoung Kim <jkiimm9@gmail.com>
2016-12-25 16:10:51 +0900

    feat(): add metadata by using yaml

--- src/scripts/Addenda.js
+++ src/scripts/Addenda.js
@@ -1,6 +1,7 @@
 import Addenda from '../components/Addenda.html';
 import request from 'superagent';
 import { markdown } from 'markdown';
+import yaml from 'js-yaml';
 
 const comp = new Addenda({
   target: document.querySelector('.body__addenda')
@@ -9,6 +10,12 @@ const comp = new Addenda({
 request.get('/assets/data/01-addenda.md')
   .then((res) => {
     comp.set({ content: markdown.toHTML(res.text) });
+  });
+
+request.get('/assets/data/01-meta.yaml')
+  .then((res) => {
+    const obj = yaml.load(res.text);
+    comp.set({ meta: markdown.toHTML(obj.addenda) });
   });
 
 export default comp;
--- src/scripts/Body.js
+++ src/scripts/Body.js
@@ -1,7 +1,16 @@
 import Body from '../components/Body.html';
+import request from 'superagent';
+import { markdown } from 'markdown';
+import yaml from 'js-yaml';
 
 const comp = new Body({
   target: document.querySelector('main')
 });
+
+request.get('/assets/data/01-meta.yaml')
+  .then((res) => {
+    const obj = yaml.load(res.text);
+    comp.set({ meta: markdown.toHTML(obj.general) });
+  });
 
 export default comp;
