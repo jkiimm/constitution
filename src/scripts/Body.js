e2215389739d8bc1b459332b9ed131dacaab3e01
Jinyoung Kim <jkiimm9@gmail.com>
2017-01-22 17:06:01 +0900

    refactor(app): use nested components way

--- src/scripts/Body.js
+++ src/scripts/Body.js
@@ -4,13 +4,38 @@ import { markdown } from 'markdown';
 import yaml from 'js-yaml';
 
 const comp = new Body({
-  target: document.querySelector('main')
+  target: document.querySelector('main'),
+  data: {
+    addenda: { content: '', meta: '' },
+  },
 });
+
+let meta = {};
+
+request.get('assets/data/10/preamble.md')
+  .then((res) => {
+    comp.set({ preamble: markdown.toHTML(res.text) });
+  });
+
+request.get('assets/data/10/provision.md')
+  .then((res) => {
+    comp.set({ provision: markdown.toHTML(res.text) });
+  });
 
 request.get('assets/data/10/meta.yaml')
   .then((res) => {
-    const obj = yaml.load(res.text);
-    comp.set({ meta: markdown.toHTML(obj.general) });
+    meta = yaml.load(res.text);
+    comp.set({ meta: markdown.toHTML(meta.general) });
+
+    return request.get('assets/data/10/addenda.md');
+  })
+  .then((res) => {
+    comp.set({
+      addenda: {
+        body: markdown.toHTML(res.text),
+        meta: markdown.toHTML(meta.addenda)
+      }
+    });
   });
 
 export default comp;
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,3 @@ import  'github-markdown-css';
 
 import './Body';
 import './Footer';
-import './Preamble';
-import './Provision';
-import './Addenda';
